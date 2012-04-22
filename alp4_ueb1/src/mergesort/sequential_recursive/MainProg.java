a020e4676da64ae2c55e7658566c6940f9698977
Daniel Spaude <daniel@spaude.de>
2012-04-22 22:40:44 +0200

    compare check of both variants result sets

--- alp4_ueb1/src/mergesort/sequential_recursive/MainProg.java
+++ alp4_ueb1/src/mergesort/sequential_recursive/MainProg.java
@@ -7,26 +7,49 @@ public class MainProg {
 	 */
 	public static void main(String[] args) {
 		
-		int[] arr = generateRandomIntArray(2000); 
+		int[] arr = generateRandomIntArray(20); 
 
+		int[] arr2 = copyArray(arr); 
+		
 		for(int val: arr)
 		{
 			System.out.println(val);
 		}
-		
-		Sort mergeSortSeqRec = new MergeSortParallelRec();
+
+		Sort mergeSortSeqRec = new MergeSortSeqRec();
 		mergeSortSeqRec.sort(arr); 
 
-		System.out.println("SORTIERT:");
+		System.out.println("SORTIERT SEQUENTIELL:");
 		for(int val: arr)
 		{
 			System.out.println(val);
 		}
 		
-				
+
+		Sort mergeSortParallelRec = new MergeSortParallelRec();
+		mergeSortParallelRec.sort(arr2); 
+
+		System.out.println("SORTIERT PARALLEL:");
+		for(int val: arr2)
+		{
+			System.out.println(val);
+		}
+		
+		
+		for(int i = 0; i < arr.length; i++)
+		{
+			if(arr[i] != arr2[i])
+			{
+				System.out.println("ERGEBNISE-ARRAYS AN STELLE "+ i + " ungleich");
+			}
+
+			else
+			{
+				System.out.println("ok an stelle "+ i); 
+			}
+		}
 		
 	}
-
 	
 	static int[] generateRandomIntArray(int size)
 	{
@@ -40,6 +63,17 @@ public class MainProg {
 		}
 		
 		return arr; 
+	}
+	
+	static int[] copyArray(int[] arr)
+	{
+		int[] arr2 = new int[arr.length];
+		
+		for(int i = 0; i < arr.length; i++)
+		{
+			arr2[i] = arr[i]; 
+		}
+		return arr2; 
 	}
 	
 }
