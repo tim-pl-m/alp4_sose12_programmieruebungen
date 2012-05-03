703b257cbe7f8426f39e1cd069732da7b953f5a2
Daniel Spaude <daniel@spaude.de>
2012-05-04 00:06:05 +0200

    work on AliceBob

---
+++ alp4_aufg11/src/AliceBob.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.atomic.AtomicBoolean;
+
+
+public class AliceBob {
+
+	AtomicBoolean alicesFlag1 = new AtomicBoolean(); 
+	AtomicBoolean alicesFlag2 = new AtomicBoolean(); 
+
+	AtomicBoolean bobsFlag1 = new AtomicBoolean(); 
+	AtomicBoolean bobsFlag2 = new AtomicBoolean(); 
+
+	AtomicBoolean runAlice = new AtomicBoolean(true); 
+	AtomicBoolean runBob = new AtomicBoolean(true); 
+	
+	
+	
+	public class Alice
+	{
+		public void runAlice()
+		{
+			while(true)
+			{
+				// GO AROUDN TOWN
+				// randomSekundenZahl zwischen 2 und 10 und für diese Zeit sleepen
+				
+				// ENTRY-SECTION
+				
+				
+			}
+			
+			
+		}
+	}
+	
+}
---
+++ alp4_aufg11/src/MainProg.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.atomic.*;
+
+
+public class MainProg {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		AtomicBoolean flag1 = new AtomicBoolean(); 
+		flag1 = true; 
+		
+		
+		
+	}
+
+}
