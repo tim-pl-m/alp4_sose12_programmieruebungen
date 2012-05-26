5ed1177607e1da2bd00cb5a449089811ad3616aa
Daniel Spaude <daniel@spaude.de>
2012-05-26 16:01:32 +0200

    work on first simple solution

--- alp4_aufg20/src/alp4/prog3/BarrierTest.java
+++ alp4_aufg20/src/alp4/prog3/BarrierTest.java
@@ -30,6 +30,9 @@ public class BarrierTest {
         }
 
         public void run() {
+        	
+        	
+        	
             System.out.println("WAITING says:" + name);
             try {
                 barrier.await();
@@ -45,9 +48,9 @@ public class BarrierTest {
 
     public void testCyclicBarrier(int[][] image, int[][] label) {
     	
+    	int numberOfWorkers = 1; 
     	
-    	
-        CyclicBarrier barrier = new CyclicBarrier(2, new Runnable() {
+        CyclicBarrier barrier = new CyclicBarrier(numberOfWorkers, new Runnable() {
                 public void run() {
                     try {
                         System.out.println("START Barrier Action ....");
@@ -59,11 +62,55 @@ public class BarrierTest {
                 }
             });
 
-        for (int i = 0; i < 1; i++) {
-            Worker worker = new Worker("Thread_"+i, image, label, barrier);
+        for (int i = 0; i < numberOfWorkers; i++) {
+            Worker worker = new Worker("Thread_"+i, image, label, 0, image[0].length, barrier);
             worker.start();
+            try {
+				worker.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
         }
 
+    }
+
+    public static void main(String[] args) {
+    	int[][] image = {
+    			{3,3,4,5}, 
+    			{5,1,2,2}, 
+    			{3,2,2,3}, 
+    			{3,3,4,5}
+    			}; 
+    	
+    	int n = image.length; 
+    	int[][] label = new int[n][n]; 
+    	
+    	
+    	System.out.println("Image: ");
+    	for(int x = 0; x < n; x++)
+    	{
+    		for(int y = 0; y < n; y++)
+    		{
+    			System.out.print(image[x][y] + " ");
+    		}
+    		System.out.println();
+    	}
+    	
+    	BarrierTest barrierTest = new BarrierTest(); 
+    	barrierTest.testCyclicBarrier(image, label); 
+    	
+    	
+    	System.out.println("Image: ");
+    	for(int x = 0; x < n; x++)
+    	{
+    		for(int y = 0; y < n; y++)
+    		{
+    			System.out.print(image[x][y] + " ");
+    		}
+    		System.out.println();
+    	}
+    	
     }
 
 
